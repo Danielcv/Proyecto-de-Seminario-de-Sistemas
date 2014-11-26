499276f9ca19b57de52536e2852ec4913bd78ae3
M Daniel Colque Vargas <M_Daniel_cv@hotmail.com>
2014-11-25 17:07:42 -0800

    Segunda presentacion del proyecto

--- tribiaOnline/tribiaOnline/apps/usuarios/forms.py
+++ tribiaOnline/tribiaOnline/apps/usuarios/forms.py
@@ -7,3 +7,10 @@ class RegistroForm(ModelForm):
 	class Meta:
 		model=Registro
 		fields=["nombre","email"]
+
+	def clean_nombre(self):
+		diccionario_limpio = self.cleaned_data
+		nombre = diccionario_limpio.get('nombre')
+		if len(nombre) <= 5:
+			raise forms.ValidationError("El nombre deve de contener mas de 4 caracteres")
+		return nombre		
